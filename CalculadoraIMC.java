89d520711df8cd5a95522b74be1ae5fe61aba862
carlosefl <106102170+carlosefl@users.noreply.github.com>
2025-01-04 16:16:54 -0300

    Update CalculadoraIMC.java
    adicionando label com detalhes

--- CalculadoraIMC.java
+++ CalculadoraIMC.java
@@ -23,24 +23,40 @@ public class CalculadoraIMC extends Application {
     // resultado do imc
 
     Label resultadoIMC = new Label();
+    Label detalhe = new Label();
 
     // Botão para iniciar o processo
 
     Button botaoCalcular = new Button("Calcular IMC");
     botaoCalcular.setOnAction(e -> {try {
-
         double peso = Double.parseDouble(campoPeso.getText().replace(',','.'));
 
     double altura = Double.parseDouble(campoAltura.getText().replace(',','.'));
     double imc = peso / (altura*altura);
     resultadoIMC.setText(String.format("seu IMC é: %.2f",imc));
+
+
+    if(imc<17){
+        detalhe.setText("Muito abaixo do peso");
+    } else if (17 >= imc && imc < 18.5) {
+        detalhe.setText("Abaixo do peso");
+    } else if (18.5 >= imc && imc < 24.99) {
+        detalhe.setText("Peso normal");
+    } else if (25 >= imc && imc < 29.99) {
+        detalhe.setText("Acima do peso");
+    } else if (30 >= imc && imc < 34.99) {
+        detalhe.setText("Obesidade I");
+    } else if (35 >= imc && imc < 39.99) {
+        detalhe.setText("Obesidade II (severa)");
+    }else {detalhe.setText("Obesidade III (mórbida)");}
+
     }catch (NumberFormatException  ex){
         resultadoIMC.setText(("Por Favor, Coloque valores validos em Altura e Peso"));
     }});
 
 
     //layout vertical
-    VBox layout = new VBox(10,etiquetaAltura,campoAltura,etiquetaPeso,campoPeso,botaoCalcular,resultadoIMC);
+    VBox layout = new VBox(10,etiquetaAltura,campoAltura,etiquetaPeso,campoPeso,botaoCalcular,resultadoIMC,detalhe);
     layout.setPadding(new Insets(10));
     layout.setAlignment(Pos.CENTER);
 
